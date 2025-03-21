ab7933190ae53a10ecc649a471dbb5d8615fb132
amoN366 <emolosimone@gmail.com>
2025-03-21 23:15:16 +0100

    updated table indentation

--- Python Script/mcStackCalculator.py
+++ Python Script/mcStackCalculator.py
@@ -1,19 +1,29 @@
+file_path = "StackCalcolati.txt"
+
+def write_header(file_path):
+    headers = (
+        "Nome Oggetto", "Quantità", "Stack Completi", "Blocchi Rimanenti", "Stack per Eccesso"
+    )
+    h = f'|{headers[0]:^20}|{headers[1]:^20}|{headers[2]:^20}|{headers[3]:^20}|{headers[4]:^20}|\n'
+    with open(file_path, "w") as f:
+        f.write('+' + ('-' * (len(h) - 2)) + '+\n')
+        f.write(h)
+        f.write('+' + ('-' * (len(h) - 2)) + '+\n')        
+
 def calcola_gruppi(nome_oggetto, qnt_blocchi, stack=64):
     stack_completi = qnt_blocchi // stack
     blocchi_rimanenti = qnt_blocchi % stack
     stack_eccesso = stack_completi + (1 if blocchi_rimanenti > 0 else 0)  # Per arrotondare in eccesso
     
-    risultato = f"{nome_oggetto}\t{qnt_blocchi}\t{stack_completi}\t{blocchi_rimanenti}\t{stack_eccesso}\n"
-    
-    with open("C:/Users/Simone/Desktop/StackCalcolati.txt", "a") as file:
+    risultato = f'|{nome_oggetto[:20]:^20}|{qnt_blocchi:^20}|{stack_completi:^20}|{blocchi_rimanenti:^20}|{stack_eccesso:^20}|\n'
+    with open(file_path, "a") as file:
         file.write(risultato)
+        file.write('+' + ('-' * (len(risultato) - 2)) + '+\n')
     
     print(f"Stack: {stack_completi}")
     print(f"Oggetti rimanenti: {blocchi_rimanenti}")
 
-
-with open("risultati.txt", "w") as file:
-    file.write("Nome Oggetto\tQuantità\tStack Completi\tBlocchi Rimanenti\tStack per Eccesso\n")
+write_header(file_path)
 
 while True:
     nome = input("Nome oggetto o 'esci' per terminare: ")
--- README.md
+++ README.md
@@ -9,11 +9,11 @@ _A python script that calculate the amount of stacks from an arbitrary amount of
 - Asks for name of the block
 - Infinite amount of items can be calculated in one session
 - save and export a table listing all the items calculated, with: name, number of stacks and number of blocks per item
+- better indentation of the table in the .txt file
 
 ### **Feature that will be added:**
 
 - Calculate the blocks from a number of stacks
 - colored text to identify the name of every item and the amount of blocks during the calculation in cmd.
 - ask for path when saving .txt file
-- better indentation of the table in the .txt file
 - set the amount of blocks per stack. E.g.  64, 16 or 1 for unstackable items
